fc1830399b0325ffc71fb174ecd489663a115119
Jaso Salgado <jaso_98@hotmail.com>
2020-06-17 17:10:24 -0500

    Base code

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'countries'
+    'rest_framework',
+    'countries',
 ]
 
 MIDDLEWARE = [
