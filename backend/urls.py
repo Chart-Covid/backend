c58ab770951019b6389c119c174aaa7df922f145
Anthony Stiven Luque <kasuru@hotmail.com>
2020-06-19 12:16:14 -0500

    <fixed>: url paths

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,7 @@ from countries import top_ten
 from countries import lon_lat_data
 
 urlpatterns = [
-    path('countries/<str:country>', countries_views.countries_stat),
     path('countries/top_ten/', top_ten.top_ten),
+    path('countries/<str:country>', countries_views.countries_stat),
     #path('countries/', lon_lat_data.all_countries),
 ]
