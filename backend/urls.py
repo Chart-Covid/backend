902c71fd7af65595aca60a19ba7ea747362b7137
Jaso Salgado <jaso_98@hotmail.com>
2020-06-22 16:26:29 -0500

    <Feature>: Countries list

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from countries import views as countries_views
 from countries import top_ten
 from countries import lon_lat_data
+from countries import show_countries
 
 urlpatterns = [
+    path('show_countries/', show_countries.show_countries),
     path('top_ten/', top_ten.top_ten),
     path('countries/<str:country>/', countries_views.countries_stat),
     #path('countries/', lon_lat_data.all_countries),
---
+++ countries/show_countries.py
@@ -0,0 +1,41 @@
+import requests
+import bs4
+
+# It does not need pandas
+
+# It looks for a country you enter
+country_name=input("Enter the Country name: ")
+
+def show_countries(country):
+    res = requests.get("https://www.worldometers.info/coronavirus/#countries")
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    index = -1
+    data=soup.select('tr td')
+    for i in range(len(data)):
+        if data[i].text.lower()==country.lower():
+            index=i
+            break
+
+    for i in range(7):
+        if i == 0:
+            print("\nCountry name: "+str(data[i+index].text))
+        elif i == 1:
+            print("Total cases: "+str(data[i+index].text))
+        elif i == 2:
+            if data[i+index].text == '':
+                print("New cases: 0")
+            else:
+                print("New cases: "+str(data[i+index].text))
+        elif i == 3:
+            print("Total deaths: "+str(data[i+index].text))
+        elif i == 4:
+            if data[i+index].text == '':
+                print("New deaths: 0")
+            else:
+                print("New deaths: "+str(data[i+index].text))
+        elif i == 5:
+            print("Total Recovered: "+str(data[i+index].text))
+        elif i == 6:
+            print("Active cases: "+str(data[i+index].text),end='\n\n')
+
+show_countries(country_name)
