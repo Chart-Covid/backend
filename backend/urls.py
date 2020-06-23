7df08ac2926902dcd0bee1bc9a126625a0c12458
Anthony Stiven Luque <kasuru@hotmail.com>
2020-06-23 11:54:49 -0500

    Estrutura inicial

--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,8 @@ from countries import top_ten, lon_lat_data, show_countries
 
 
 urlpatterns = [
-    path('show_countries/', show_countries.show_countries),
+    #path('show_countries/', show_countries.show_countries),
     path('top_ten/', top_ten.top_ten),
-    path('countries/<str:country>/', countries_views.countries_stat),
+    path('countries/<str:country>/', show_countries.show_countries),
     #path('countries/', lon_lat_data.all_countries),
 ]
